1c3eef4ed46b053aa8eadb2f06396c4cd95fb932
JiiXaa <anythingfx@gmail.com>
2022-04-17 18:19:17 +0100

    extracted debounce to separate function

--- index.js
+++ index.js
@@ -11,17 +11,25 @@ const fetchData = async (searchTerm) => {
   console.log(response.data);
 };
 
-// variable to store setTimeout id, for clearing it when needed
-let timeoutId;
+const debounce = (callback, delay = 1000) => {
+  // variable to store setTimeout id, for clearing it when needed
+  let timeoutId;
 
-// simple debounce with setting timeout with first click, and next will clear it until you stop typing inside input. When you stop it will fetch data.
-const onInputHandler = (e) => {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
-  timeoutId = setTimeout(() => {
-    fetchData(e.target.value);
-  }, 500);
+  // simple debounce with setting timeout with first click, and next will clear it until you stop typing inside input. When you stop it will fetch data.
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      // apply keep track of how many attributes need to be passed to the callback function (can use spread ...args instead of passing every argument one by one)
+      callback.apply(null, args);
+    }, delay);
+  };
 };
 
-input.addEventListener('input', onInputHandler);
+// Function handling search functionality with inside input
+const onInputHandler = (e) => {
+  fetchData(e.target.value);
+};
+
+input.addEventListener('input', debounce(onInputHandler, 500));
