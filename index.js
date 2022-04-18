de016988206235a4394b7fd9b9c4f2ef884372d0
JiiXaa <anythingfx@gmail.com>
2022-04-18 18:51:42 +0100

    display search result in DOM

--- index.js
+++ index.js
@@ -8,12 +8,28 @@ const fetchData = async (searchTerm) => {
     },
   });
 
-  console.log(response.data);
+  // omdbapi does not accept partial search term, and get data.Error instead of data.Search which looks like an error. That's why we return [] if response has data.Error
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
 };
 
 // Function handling search functionality with inside input
-const onInputHandler = (e) => {
-  fetchData(e.target.value);
+const onInputHandler = async (e) => {
+  const movies = await fetchData(e.target.value);
+
+  for (let movie of movies) {
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+    <img src="${movie.Poster}" />
+    <h1>${movie.Title}</h1>
+    `;
+
+    document.querySelector('#here-temp').appendChild(div);
+  }
 };
 
 input.addEventListener('input', debounce(onInputHandler, 500));
