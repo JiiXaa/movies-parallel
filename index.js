219fc6d8a6b7332918114e24c4000d57e92eb7d9
JiiXaa <anythingfx@gmail.com>
2022-05-04 21:33:00 +0100

    displaying multiple autocompletes for movie comparision

--- index.js
+++ index.js
@@ -15,66 +15,14 @@ const fetchData = async (searchTerm) => {
   return response.data.Search;
 };
 
-const root = document.querySelector('.autocomplete');
-// dynamically created search component
-root.innerHTML = `
-<label><b>Search For a Movie</b></label>
-<input class="input" />
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-// Function handling search functionality with inside input
-const onInputHandler = async (e) => {
-  const movies = await fetchData(e.target.value);
-
-  // make sure empty dropdown is not shown if there is no search results.
-  if (!movies.length) {
-    dropdown.classList.remove('is-active');
-    return;
-  }
-
-  resultsWrapper.innerHTML = '';
-  dropdown.classList.add('is-active');
-
-  for (let movie of movies) {
-    const option = document.createElement('a');
-    // Do not show image if there is not one. Omdb API has N/A string if there is no image available.
-    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
-    option.classList.add('dropdown-item');
-    option.innerHTML = `
-    <img src="${imgSrc}" />
-    ${movie.Title}
-    `;
-    // listening for a click on every 'a' element (logic for clicking search entry)
-    option.addEventListener('click', () => {
-      // when click on the 'a' element close dropdown
-      dropdown.classList.remove('is-active');
-      // because of a closure, we have access to the movie object
-      input.value = movie.Title;
-      // helper function, logic when you choose a movie
-      onMovieSelect(movie);
-    });
-
-    resultsWrapper.appendChild(option);
-  }
-};
-
-input.addEventListener('input', debounce(onInputHandler, 500));
-
-// Simple way to close search dropdown when clicked outside of root element.
-document.addEventListener('click', (e) => {
-  if (!root.contains(e.target)) {
-    dropdown.classList.remove('is-active');
-  }
+createAutoComplete({
+  root: document.querySelector('.autocomplete'),
+});
+createAutoComplete({
+  root: document.querySelector('.autocomplete-two'),
+});
+createAutoComplete({
+  root: document.querySelector('.autocomplete-three'),
 });
 
 // second data fetch for more detailed information, after film is chosen
